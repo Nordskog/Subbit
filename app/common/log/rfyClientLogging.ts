42577d6ef26f9c283e68c96e61f322cc531271e4
Roughy <mdawn700@gmail.com>
2018-07-15 02:11:11 +0200

    Fixed client side logging functions to match server

--- app/common/log/rfyClientLogging.ts
+++ app/common/log/rfyClientLogging.ts
@@ -1,19 +1,35 @@
-  export function I( message : any)
-  {
-    console.log(message);
-  }
+import { Severity } from './models';
 
-  export function E( message : any)
-  {
-    console.log(message);
-  }
+export function I( message : any, meta : Object = {})
+{
+  console.log(message, meta);
+}
 
-  export function D( message : any)
-  {
-    console.log(message);
-  }
+export function A( message : any, user : string, ip : string,  meta : Object = {})
+{
+  meta = {  ... meta, 
+            user: user, 
+            ip: ip
+        } as any;
+  console.log( message, meta);
+}
 
-  export function W( message : any)
-  {
-    console.log(message);
-  }
+export function E( message : any, meta : Object = {})
+{
+  console.error(message, meta);
+}
+
+export function D( message : any, meta : Object = {})
+{
+  console.debug(message, meta);
+}
+
+export function W( message : any, meta : Object = {})
+{
+  console.warn(message, meta);
+}
+
+export function L( severity : Severity, message : any, meta : Object = {})
+{
+  console.log( severity, message, meta);
+}
