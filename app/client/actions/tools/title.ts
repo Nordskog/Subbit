8662ea1d402ed4ba59b476e22045f3f2297d0a2e
Roughy <mdawn700@gmail.com>
2018-08-09 20:22:04 +0200

    Added "for Reddit" to all titles to make more easily recognizable

--- app/client/actions/tools/title.ts
+++ app/client/actions/tools/title.ts
@@ -9,34 +9,38 @@ export function updateTitle( getState : GetState)
     //Test if non-standard route, of which there currently are none
     //Apart from specific sections, we are only going to bother with subreddits and authors.
     //Otherwise keep title generic, with filter if present.
+
+    //"for Reddit" added to make more recognizable in search results
     let state : State = getState();
 
     if (state.siteState.mode == models.SiteMode.STATS)
     {
-        document.title = config.client.siteName+" - Stats"
+        document.title = config.client.siteName+" for Reddit - Stats"
     }
     else if (state.authorState.author != null)
     {
         if (state.authorState.subreddit != null)
         {
-            document.title = config.client.siteName+" - "+state.authorState.author+" in "+state.authorState.subreddit;
+            document.title = config.client.siteName+" for Reddit - "+state.authorState.author+" in r/"+state.authorState.subreddit;
         }
         else
         {
-            document.title = config.client.siteName+" - "+state.authorState.author;
+            document.title = config.client.siteName+" for Reddit - "+state.authorState.author;
         }
     }
     else if (state.authorState.subreddit != null)
     {
-        document.title = config.client.siteName+" - "+state.authorState.subreddit;
+        document.title = config.client.siteName+" for Reddit - "+state.authorState.subreddit;
     }
+    /*
     else if (state.authorState.filter != null)
     {
-        document.title = config.client.siteName+" - "+getFilterDisplayString(state.authorState.filter);
+        document.title = config.client.siteName+" for Reddit - "+getFilterDisplayString(state.authorState.filter);
     }
+    */
     else
     {
-        document.title = config.client.siteName;
+        document.title = config.client.siteName + " for Reddit";
     }
     
 } 
