1b1aae6d9270b04166a07f1a6e012a30be4c2367
Roughy <mdawn700@gmail.com>
2018-08-30 06:44:51 +0200

    Fixed InfoPopup alignment, disabled on mobile

--- app/client/components/tools/InfoPopup.tsx
+++ app/client/components/tools/InfoPopup.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import * as components from '~/client/components';
 import * as siteStyles from 'css/site.scss';
 
+import MediaQuery from 'react-responsive';
+
 interface Props
 {
     text : string;
@@ -17,23 +19,42 @@ export default class InfoPopup extends React.Component<Props, State>
 {
     public render()
     {
-        return <components.tools.Popup.Component
-            trigger={this.props.trigger }
-            hover={true}
-            showBackground={true}
-            hoverAppearDelay={0.3}
-            position={ components.tools.Popup.Position.TOP}
-            alignment={ components.tools.Popup.Alignment.BEGINNING }>
+        // Mobile doesn't handle hover out events at all.
+        // Disable on mobile.
+        return <MediaQuery query="screen and (max-width: 1100px)">
         {
-            (close) => 
+            (matches : boolean) => 
             {
-                    return  <div className={siteStyles.InfoPopup}>
-                                {this.props.text}
-                                <div className={siteStyles.infoPopupArrow} />
-                            </div>;
-            }
+                if (matches)
+                {
+                    return this.props.trigger;
+                }
+                else
+                {
+                    return <components.tools.Popup.Component
+                        trigger={this.props.trigger }
+                        hover={true}
+                        showBackground={true}
+                        hoverAppearDelay={0.3}
+                        position={ components.tools.Popup.Position.TOP}
+                        alignment={ components.tools.Popup.Alignment.BEGINNING }>
+                    {
+                        (close) => 
+                        {
+                                return  <div className={siteStyles.InfoPopup}>
+                                            {this.props.text}
+                                            <div className={siteStyles.infoPopupArrow} />
+                                        </div>;
+                        }
+                    }
+            
+                    </components.tools.Popup.Component>;
+                }
+            } 
         }
+        </MediaQuery>;
 
-        </components.tools.Popup.Component>;
+
+
     }
 }
